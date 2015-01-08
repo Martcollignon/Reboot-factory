2e8c04f6bc49d13d4ba1cf21e9c0af8fe1fcf71c
Bluwv <adrienpierre@aol.fr>
2015-01-08 17:46:36 +0100

    local storage, ajout article et plan

--- _script/main.js
+++ _script/main.js
@@ -1,23 +1,63 @@
 $(document).ready(function(){
 
-/* SCRIPT AUTOCOMPLETE LISTE */
+// Autocomplete
 
-	var data = [
-				"thon",
-				"carotte",
-				"steak",
-				"eau",
-				"coca"
-				];
+	// var data = [
+	// 			"thon",
+	// 			"carotte",
+	// 			"steak",
+	// 			"eau",
+	// 			"coca",
+	// 			"cotelette de poule",
+	// 			"saumon",
+	// 			"pain blanc"
+	// 			];
 	  
-	$( "#produit" ).autocomplete({
-	  source:data,
-	  autoFocus: true ,
+	// $( "#addproduct" ).autocomplete({
+	//   source:data,
+	//   autoFocus: true ,
 
+	// });
+
+	$('.section__banner').click(function(){
+		$('.section__banner').toggleClass("active");
 	});
 
-	$('.list--element').click(function(){
-		$(this).remove();
-	});
+// Supprimer element liste
+
+	// $('.list--small__delete').click(function(){
+	// 	$(this).parent().remove();
+	// 	localStorage.removeItem();
+	// });
+
+//
+
+// Naviguer avec les listes dans le plan
+
+	$(".course").on("click", function(){
+    
+        var el = $(".liste").children().not(".liste__el--achete").first();
+        el.children("input").prop('checked', true);
+        el.addClass("liste__el--achete").hide();
+        
+    });
+    
+    $(".check").on("click", function(){
+        if($(this).prop('checked')){
+            $(this).parent().addClass("liste__el--achete").hide();
+        } else {
+            $(this).parent().removeClass("liste__el--achete").show();
+        }
+    });
+    
+    $( ".afficher" ).toggle(function() {
+      $(".liste__el--achete").show();
+    }, function() {
+      $(".liste__el--achete").hide();
+    });
+    
+    $(".delete").on("click", function(){
+        $(this).parent().remove();
+    });
 
 });
